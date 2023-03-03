1b57f129f435dd05561845da6d4e019f5c033b2a
julianmedwards <julian.michael.edwards@gmail.com>
2023-03-03 13:45:37 -0600

    Removed routing to REST server- client will call directly.

--- server.js
+++ server.js
@@ -1,29 +1,14 @@
 'use strict'
 
 const express = require('express')
-// const cors = require('./node_modules/cors')
 
 const app = express()
 const port = process.env.PORT || 3000
-const router = require('./routes/index')
-
-// const allowedOrigins = ['http://localhost:3000']
-
-// const corsOptions = {
-//     origin: allowedOrigins,
-// }
-
-// app.use(cors(corsOptions))
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
 
 app.set('views', './views')
 app.set('view engine', 'pug')
-
-app.use('/', router)
-
-global.__basedir = __dirname
 
 app.get('/', async (req, res) => {
     const response = await fetch('http://localhost:5000/lanes', {
